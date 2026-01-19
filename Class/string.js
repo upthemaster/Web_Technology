ad4d2993abab56c5e8f8339a3e4d4ce0b4658d7b
upthemaster <upthemaster@gmail.com>
2026-01-19 15:45:53 +0530

    string.js added in Class

---
+++ Class/string.js
@@ -0,0 +1,20 @@
+// Concatenation
+const str1 = "WebTech"
+console.log(str1);
+console.log("Hello " + str1);
+
+// Interpolation
+// It is optimal way to print Variables
+console.log(`Hello my course name is ${str1}`); // While using interpolation we have to use $ symbol with backstrip``
+
+const gameCount = new String("Master"); // new is used to create object
+console.log(gameCount.length);
+console.log(gameCount[3]); // returns the string of particular index
+console.log(gameCount.toUpperCase()); // convert string into Uppercase
+console.log(gameCount.toLowerCase()); // convert string into Lowercase
+console.log(gameCount.charAt(3)); // returns the string of particular index
+console.log(gameCount.indexOf("t")); // returns index of particular string
+
+const fName = "  Utkarsh  "
+console.log(fName.trim()); // removes extra space and return plain output.
+
